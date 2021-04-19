476cca71f0e085faba33a17615d8b40290ecb1b9
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-19 14:07:50 +0200

    Aggiunto layout footer

--- js/script.js
+++ js/script.js
@@ -2,10 +2,29 @@ var app = new Vue (
   {
     el: "#app",
     data: {
-
+      listaFooterTop: [
+        "Home",
+        "Meet The Band",
+        "Live Dates",
+        "Latest News",
+        "Albums",
+        "Fans"
+      ],
+      listaFooterBottom: [
+        "© Copyright 2012-2020",
+        "AVADA THEME BY THEMEFUSION",
+        "ALL RIGHTS RESERVED",
+        "POWERED BY WORDPRESS",
+      ],
+      listaIconeSocial: [
+        "fa-facebook-f",
+        "fa-twitter",
+        "fa-instagram",
+        "fa-youtube"
+      ]
     },
     mounted: function() {
-      
+
     },
     methods: {
 
